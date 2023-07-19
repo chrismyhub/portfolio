c744925e6686ecb695b78a509e0d4c9fa4fbf7d3
chrismyhub <keyoch@gmail.com>
2023-07-19 04:29:52 +0000

    2.2 create a secret password exercise added password hidden keystrokes and converted secretPassword to Hash in secret.txt

--- week2/setPassword.sh
+++ week2/setPassword.sh
@@ -4,13 +4,16 @@
 read -p "Please type a folder name: " folderName
 
 #User to type in a secret password and save it as a variable secretPassword
-read -p "Please type a Secret Password: " secretPassword
+read -rsp "Please type a Secret Password: " secretPassword
 
 #This creates a new folder with folderName as the name
 mkdir "$folderName"
 
+#This will convert secretPassword into a Hash and then save it in a file named secret.txt and save it inside a folder named folderName
+echo -n "$secretPassword" | sha256sum > ./"$folderName"/secret.txt
+
 #This will save the user's secretPassword into a file named secret.txt inside the folder named folderName
-echo "$secretPassword" > ./"$folderName"/secret.txt
+# echo "$secretPassword" > ./"$folderName"/secret.txt
 
 #Test to see if variable folderName is working
 echo $folderName
