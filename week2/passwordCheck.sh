a73525f71701427457729c92ca36f1f36b9444cf
chrismyhub <keyoch@gmail.com>
2023-07-20 05:23:10 +0000

    2.4 passwordCheck.sh file created with template of what code needs to do

---
+++ week2/passwordCheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#Ask the user to type a secret password (hide that input).
+
+#Check the user’s password against the hash stored in 'secret.txt'.
+
+#If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
+
+#If the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
+
+#Exit code
+exit 0
