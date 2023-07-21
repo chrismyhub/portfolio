9f4196b99c0c8255bf1affebc3258e64fc4dc8df
chrismyhub <keyoch@gmail.com>
2023-07-21 01:58:12 +0000

    2.4 passwordCheck.sh If statement code added to check password entered against stored Hash in secret.txt and exit codes included if access granted or denied.

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -4,11 +4,26 @@
 #Prompt user to type a secret password (hide that input) and save as a variable.
 read -rsp "Please enter a Secret Password: " secretPassword
 
-#Check the user’s password against the hash stored in 'secret.txt'.
+#Add space for resulting message to user
+echo
 
-#If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
+#Check the user’s password against the hash stored in 'secret.txt'and if the user’s password is correct, print “Access Granted” and quit with the exit code 0.
+if [[ $(echo -n "$secretPassword" | sha256sum -c secret.txt) = "-: OK" ]]
+then
 
-#If the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
+    #Display Access Granted to user
+    echo "Access Granted"
 
-#Exit code
-exit 0
+    #Exit code 
+    exit 0
+
+#Check the user’s password against the hash stored in 'secret.txt' and if the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
+elif [[ $(echo -n "$secretPassword" | sha256sum -c secret.txt) = "-: FAILED" ]]
+then
+
+    #Display Access Denied to user
+    echo "Access Denied"
+    
+    #Exit code
+    exit 1
+fi 
