9caae3340b09f0a9803da11cc76f5e2439f41588
chrismyhub <keyoch@gmail.com>
2023-07-23 14:35:01 +0000

    2.6 fixed errors in errors.sh and added random secret menu if secret code was entered correctly.

--- week2/errors.sh
+++ week2/errors.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
       
 secret='shhh' #Don't tell anyone! 
-      
+
+#Prompt user to enter the secret code and save it as a variable
+read -s -p "what's the secret code?"  REPLY
+
+#New line to format next section of code clearer
+echo
+
 #if the user types in the correct secret, tell them they got it right! 
       
 if [ "$secret" = $REPLY ]; then 
@@ -16,26 +22,28 @@ else     echo "You got it wrong :("
       
 fi 
       
-read -s -p "what's the secret code?" 
+
       
 echo 
       
 case $correct in 
+
+true)       
+echo "You have unlocked the secret menu!" 
+echo      
+#TODO: add a secret menu for people in the know. 
+echo "WELCOME TO THE SECRET MENU..."
+echo "1. Forecast for tomorrow."
+echo "2. Seven day forecast."
+echo "3. Six month forecast."      
+;; 
       
-    *) 
+*) 
       
-    echo "Go Away!" #people who get it wrong need to be told to go away! 
+echo "Go Away!" #people who get it wrong need to be told to go away! 
       
-    ;; 
-      
-    true) 
-      
-    echo "you have unlocked the secret menu!" 
-      
-    #TODO: add a secret menu for people in the know. 
-      
-    ;; 
-      
+;; 
+          
 esac 
      
      
