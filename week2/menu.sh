005e98534f2c63c8e2a1efd7e985d73aa86c1c66
chrismyhub <keyoch@gmail.com>
2023-07-23 01:35:48 +0000

    2.5 menu.sh added code to run passwordCheck.sh and if exit code is 0 then display menu.sh, otherwise exit script

--- week2/menu.sh
+++ week2/menu.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-#Display menu options
-echo "1. Create a folder"
-echo "2. Copy a folder"
-echo "3. Set a password"
+#Run passwordCheck script
+./passwordCheck.sh
+
+#Check if exit code is "0" then display menu
+if [ $? -eq 0 ]; then
+
+    #Provide line space for menu
+    echo
+
+    #Display menu options
+    echo "1. Create a folder"
+    echo "2. Copy a folder"
+    echo "3. Set a password"
+else
+    #If exit code is "1" then exit script
+    echo ""
+fi
+
 
 #Exit code
 exit 0
