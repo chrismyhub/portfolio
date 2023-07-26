d671c83a9bd092a7b8642bc303e030f11e99e7de
chrismyhub <keyoch@gmail.com>
2023-07-26 14:39:27 +0000

    3.1 snippetTest.sh testing shebang and case snippets.

---
+++ week3/snippetTest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+var=#_ENTER A VARIABLE HERE_
+case $var in
+#_TEST ITEM_)
+# _CODE IF MATCH_
+#End of case statement 1
+;;
+#_TEST ITEM_)
+# _CODE IF MATCH_
+#End of case statement 2
+;;
+#_TEST ITEM_)
+# _CODE IF MATCH_
+#End of case statement 3
+;;
+#_TEST ITEM_)
+# _CODE IF MATCH_
+#End of case statement 4
+;;
+#_TEST ITEM_)
+# _CODE IF MATCH_
+#End of case statement 5
+;;
+*) #Default case
+# _CODE IF DEFAULT CASE_
+#End of default case statement
+;;
+esac
