3cb3c0303d6fb87df62107a817948c832eb761a1
chrismyhub <keyoch@gmail.com>
2023-07-28 03:27:06 +0000

    3.1 step 3 shell color variables exercise, added listed colors in shellscript.json for easy access to color variables when needed.

--- week3/snippetTest.sh
+++ week3/snippetTest.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-var=#_ENTER A VARIABLE HERE_
-case $var in
-#_TEST ITEM_)
-# _CODE IF MATCH_
-#End of case statement 1
-;;
-#_TEST ITEM_)
-# _CODE IF MATCH_
-#End of case statement 2
-;;
-#_TEST ITEM_)
-# _CODE IF MATCH_
-#End of case statement 3
-;;
-#_TEST ITEM_)
-# _CODE IF MATCH_
-#End of case statement 4
-;;
-#_TEST ITEM_)
-# _CODE IF MATCH_
-#End of case statement 5
-;;
-*) #Default case
-# _CODE IF DEFAULT CASE_
-#End of default case statement
-;;
-esac
 
-if [[ _AND TEST_ ]] && ( [[ _OR OPTION 1_ ]] || [[ _OR OPTION 2_ ]] ); then
-#_CODE IF TRUE_
-else
-#_CODE IF FALSE_
-fi
+black_color='\033[30m'
+red_color='\033[31m'
+green_color='\033[32m'
+brown_color='\033[33m'
+blue_color='\033[34m'
+purple_color='\033[35m'
+cyan_color='\033[36m'
+grey_color='\033[37m'
+NC='\033[0m'
+
+printf "I am ${black_color}black${NC} color\n"
+printf "I am ${red_color}red${NC} color\n"
+printf "I am ${green_color}green${NC} color\n"
+printf "I am ${brown_color}brown${NC} color\n"
+printf "I am ${blue_color}blue${NC} color\n"
+printf "I am ${purple_color}purple${NC} color\n"
+printf "I am ${cyan_color}cyan${NC} color\n"
+printf "I am ${grey_color}grey${NC} color\n"
