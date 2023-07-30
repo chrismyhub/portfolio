b54568906d8b9c7c5cac9c7c2a45fa5e850d18de
chrismyhub <keyoch@gmail.com>
2023-07-30 14:52:12 +0000

    3.5 InternetDownloader.sh updated with downloading code using wget to download URL link to a specific location.

---
+++ week3/InternetDownloader.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Start of while loop
+while true
+    do
+        #Prompt user to enter a URL or quit
+        read -rp "Please type the URL of a file to download or type 'exit' to quit: " URL
+    
+        #If user types exit, then quit the script immidiately...
+        if [[ $URL = exit ]]
+            then 
+                exit 0
+        fi
+        
+        #...otherwise continue by prompting user to enter the URL for downloading
+        read -rp "Type the location of where you would like to download the file to: " LOCATION
+
+        #Download file from $URL and save it to $LOCATION    
+        wget -P "$LOCATION" "$URL"
+
+    #End of while loop
+    done
