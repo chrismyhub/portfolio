7c993225d394eebfff0f2b49c5e75d939757318a
chrismyhub <keyoch@gmail.com>
2023-08-01 04:20:48 +0000

    4.1 Called on pretty_printing.sh script from program.sh script for text formatting and then attach an argument to the called script to display the output.

--- week4/program.sh
+++ week4/program.sh
@@ -6,3 +6,10 @@ echo -e "message: \033[32mhello\033[00m"
 echo -e "message: \033[32mThis is an important message!\033[00m"
 
 echo -e "message: \033[32mhello\033[00m"
+
+#To output the same as above, however here we are calling on pretty_printing.sh for the formatting and simply attach an argument
+./pretty_printing.sh "hello"
+
+./pretty_printing.sh "This is an important message!"
+
+./pretty_printing.sh "hello"
