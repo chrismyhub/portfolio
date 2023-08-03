d9d4a1bd43b70f25e0c8f34e38a2dfea84334eb0
chrismyhub <keyoch@gmail.com>
2023-08-03 04:43:42 +0000

    Added Creator, Date created and last modified to foldermaker.sh.

--- week2/foldermaker.sh
+++ week2/foldermaker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#CREATED BY: Christopher Chong
+#DATE CREATED: 17JUL2023
+#DATE LAST MODIFIED: 03AUG2023
 
 read -p "type the name of the folder you would like to create: " folderName
 
