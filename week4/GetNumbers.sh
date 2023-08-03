303019e4494a1850b89f1dfb778be7137d028c9f
chrismyhub <keyoch@gmail.com>
2023-08-03 08:32:53 +0000

    4.2 Step 1, created GetNumbers.sh and copied over code from exercise.

---
+++ week4/GetNumbers.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#CREATED BY: Christopher Chong
+#DATE CREATED: 02AUG2023
+#DATE LAST MODIFIED: 02AUG2023
+      
+ 
+      
+#This function prints a given error 
+      
+printError() 
+      
+{ 
+      
+    echo -e "\033[31mERROR:\033[0m $1" 
+      
+} 
+      
+ 
+      
+#This function will get a value between the 2nd and 3rd arguments 
+      
+getNumber() 
+      
+{ 
+      
+    read -p "$1: " 
+      
+    while (( $REPLY < $2 || $REPLY> $3 )); do 
+      
+        printError "Input must be between $2 and $3" 
+      
+        read -p "$1: " 
+      
+    done 
+      
+} 
+      
+ 
+      
+echo "this is the start of the script" 
+      
+getNumber "please type a number between 1 and 10" 1 10 
+      
+echo "Thank you!" 
+      
+getNumber "please type a number between 50 and 100" 50 100 
+      
+echo "Thank you!" 
+     
+					
