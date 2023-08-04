883423b796f663f390c6e9b6c3c35fc466f15029
chrismyhub <keyoch@gmail.com>
2023-08-04 05:07:58 +0000

    4.3 GuessingGame.sh updated notNumber() function to include checking if only the ENTER key is pressed and also that it is only integers being entered.

--- week4/GuessingGame.sh
+++ week4/GuessingGame.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CREATED BY: Christopher Chong
 #DATE CREATED: 02AUG2023
-#DATE LAST MODIFIED: 02AUG2023
+#DATE LAST MODIFIED: 04AUG2023
 
 #This will print the start of an error message in red    
 printError() 
@@ -16,47 +16,48 @@ printError()
 #Function to determine what error to display to user   
 notNumber() 
       
-{   
-    read -p "$1: " 
-    #If the number is less than 42, it will display "Too Low!"   
-    while (( $REPLY < $2 || $REPLY > $2 )); do 
+{   #Passing through the first argument of notNumber and also setting up ENTER key being pressed
+    read -p "$1: " || read -s -n 1 key
 
-        if [[ $REPLY -lt $2 ]]
-            then
+    #Start of while loop, defining if user input is less than or greater than the second parameter (in this case number 42) and also that the user input is not equal to number 42.
+    while (( REPLY < $2 || REPLY > $2  && REPLY != $2 )); do
+
+        #If only ENTER key is pressed and nothing else, instruct user to enter an integer
+        if [[ $key = "" && $REPLY = "" ]]
+        then 
+            printError "Please enter an integer!"
+
+        #If the number is less than 42, it will display "Too Low!" 
+        #The "$REPLY =~ ^[0-9]+$ " checks that it is only an integer entered
+        elif [[ $REPLY -lt $2 && $REPLY =~ ^[0-9]+$ ]]
+        then
             printError "Too Low!" 
 
-        elif [[ $REPLY -gt $2 ]]
+        #If the number is more than 42, it will display "Too High!" 
+        #The "$REPLY =~ ^[0-9]+$ " checks that it is only an integer entered
+        elif [[ $REPLY -gt $2 && $REPLY =~ ^[0-9]+$ ]] 
         then
             printError "Too High!" 
-        #If ENTER is pressed ~NOT WORKING
-        elif [[ -z $VAR ]]
-        then printError "Please enter an integer!"
-        
+
+        #If none of the above conditions are met, instruct user to input an integer
         else
             printError "Please enter an integer!"
         fi
         
         read -p "$1: " 
-      
+        
     done 
 
-    #If the number is more than 42, it will display "Too High!"  
-    # while (( $REPLY > $2 )); do 
-      
-    #     printError "Too High!" 
-        
-    #     read -p "$1: " 
-      
-    # done
-      
 } 
 
 
-
+#Welcome message when scrip tis run.
 echo "Welcome to the Guessing Game!" 
-
-echo "Try to guess the number I am thinking of..."
-      
+#New line to space out welcome message.
+echo
+#Instruct user to enter a number
+echo "Try to guess the number I am thinking of..."     
 notNumber "...type your guess here: " 42
-      
+
+#When user has entered the corect number, the while loop is complete and the following message will appear.
 echo "Correct!" 
