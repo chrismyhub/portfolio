75a818357066dedbb82a6456f41ad54c4e63d998
chrismyhub <keyoch@gmail.com>
2023-08-07 05:52:30 +0000

    4.5 sending ifconfig through to sed using pipes.

--- week4/ip_info.sh
+++ week4/ip_info.sh
@@ -2,3 +2,10 @@
 #CREATED BY: Christopher Chong
 #DATE CREATED: 07AUG2023
 #DATE LAST MODIFIED: 07AUG2023
+
+
+#Set variable for ifconfig command
+net_info="$(ifconfig)"
+
+#Replace inet with IP Address
+echo $net_info | sed 's/inet/IP Address:/' 
