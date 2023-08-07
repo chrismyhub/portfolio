ad69632b1e040eb53480a24902899e0d0676f834
chrismyhub <keyoch@gmail.com>
2023-08-07 06:03:37 +0000

    4.5 ip_info.sh added double quotes to $net_info variable.

--- week4/ip_info.sh
+++ week4/ip_info.sh
@@ -9,7 +9,7 @@ net_info="$(ifconfig)"
 
 #Replace inet with IP Address, netmask with Subnet Mask and broadcast with Broadcast Address.
 #Using -n will not output sed unless using print command.
-echo $net_info | sed -n '/inet / {
+echo "$net_info" | sed -n '/inet / {
     s/inet/IP Address:/
     s/netmask/Subnet Mask:/
     s/broadcast/Broadcast Address/
