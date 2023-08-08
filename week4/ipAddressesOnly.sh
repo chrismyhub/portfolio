f5884fb9dc6f1aad666a850b7bda1e055cafcc87
chrismyhub <keyoch@gmail.com>
2023-08-08 02:24:23 +0000

    4.5 ipAddressesOnly.sh added code to convert .sh file to .txt and filter for "IP Address" prefix then deleting .txt files used.

--- week4/ipAddressesOnly.sh
+++ week4/ipAddressesOnly.sh
@@ -3,5 +3,20 @@
 #DATE CREATED: 08AUG2023
 #DATE LAST MODIFIED: 08AUG2023
 
-#Run ip_info.sh script
-/home/student/student/scripts/portfolio/week4/ip_info.sh
+#Run ip_info.sh script and save as a variable
+ipRaw=$(/home/student/student/scripts/portfolio/week4/ip_info.sh)
+
+#Convert .sh script into .txt file.
+echo -n "$ipRaw" >> test.txt
+
+#Remove all spaces on each line.
+awk '{$1=$1};1' < test.txt >> test2.txt
+
+#Filter out lines with prefix of "IP Address" and display to user.
+sed -n -e '/^IP Address/p' /home/student/student/scripts/portfolio/week4/test2.txt
+
+#Remove test.txt file.
+rm test.txt
+
+#Remove test2.txt file.
+rm test2.txt
