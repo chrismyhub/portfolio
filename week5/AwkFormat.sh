6aaef6e0fd951e3c0439f351624aa51b8694d534
chrismyhub <keyoch@gmail.com>
2023-08-11 04:24:51 +0000

    5.4 step 4, Updated AwkFormat.sh with more advanced code to tabulate the input.txt file with colored text.

--- week5/AwkFormat.sh
+++ week5/AwkFormat.sh
@@ -1,11 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
+#CREATED BY: Christopher Chong
+#DATE CREATED: 11AUG2023
+#DATE LAST MODIFIED: 11AUG2023
       
 echo "Google Server IPs:" 
       
-awk '{ 
+awk 'BEGIN { 
       
-    print $1 
+    FS=":"; 
+      
+    print "________________________________"; 
+      
+    print "| \033[34mServer Type\033[0m | \033[34mIP\033[0m |"; 
+      
+} 
+      
+{ 
+      
+    printf("| \033[33m%-11s\033[0m | \033[35m%-14s\033[0m |\n", $1, $2); 
+      
+} 
+      
+END { 
+      
+    print("________________________________"); 
       
 }' input.txt 
-      
+     
 					
