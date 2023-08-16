814cc469a69f546d2dee6373d9cf4e388b302794
chrismyhub <keyoch@gmail.com>
2023-08-16 01:31:23 +0000

    6.4 Step 3, added print code in pythonExample.py.

---
+++ week6/pythonExamples.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# CREATED BY: Christopher Chong
+# DATE CREATED: 16AUG2023
+# DATE LAST MODIFIED: 16AUG2023 
+
+print("Hi there!")
+
+value = "Blah"
+
+print("The value is %s" % value)
