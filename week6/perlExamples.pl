e4be2e67936b423ac17963e6d24256f822b6815c
chrismyhub <keyoch@gmail.com>
2023-08-16 02:56:51 +0000

    6.4 Step 5, Added code to test REGEX in perl.

--- week6/perlExamples.pl
+++ week6/perlExamples.pl
@@ -12,3 +12,10 @@ $x=$x+10;
 print "Hello there\n";
 print "This is more text and the value is $value\n";
 print "The value of x is $x\n";
+
+if( $value =~ m/.*ah$/ ) {
+    print("value ends with the letters ah\n");
+}
+else {
+    print("value does not end with the letters ah\n");
+}
