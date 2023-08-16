a25d68e2ab3d11c6eaf03ace539c6753e55d265a
chrismyhub <keyoch@gmail.com>
2023-08-16 03:59:09 +0000

    6.5 Step 2, added dictionary attack code.

--- week6/ DictionaryAttack.sh
+++ week6/ DictionaryAttack.sh
@@ -2,3 +2,21 @@
 #CREATED BY: Christopher Chong
 #DATE CREATED: 16AUG2023
 #DATE LAST MODIFIED: 16AUG2023
+
+#hidden password hash
+passwordHash="8b7df143d91c716ecfa5fc1730022f6b421b05cedee8fd52b1fc65a96030ad52";
+#for each line in the file
+for word in $(cat wordlist.txt); do
+    
+    #hash the word
+    wordlistHash=$(echo -n $word | sha256sum | awk '{ print $1; }')
+    echo "Trying $word: $wordlistHash"
+    
+    #if the hash is the same as the correct password's hash then we have cracked the password!
+    if [ $wordlistHash = $passwordHash ]; then
+        echo "Password has been cracked! It was $word"
+        exit 0
+    fi
+done
+
+exit 1
