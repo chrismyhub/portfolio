a7216c6a554163a636ba5eef3056c1ac69a90be1
chrismyhub <keyoch@gmail.com>
2023-08-16 04:23:21 +0000

    6.5 Step 8, Updated code with "while" loop to make it work.

--- week6/DictionaryAttack.pl
+++ week6/DictionaryAttack.pl
@@ -7,9 +7,10 @@ use Digest::SHA qw(sha256_hex);
 
 #read the password hash
 $passwordHash = "04e77bf8f95cb3e1a36a59d1e93857c411930db646b46c218a0352e432023cf2";
-
 #open the wordlist
-open(WORDLIST, ") {
+open(WORDLIST, "wordlist.txt"); 
+
+while (<WORDLIST>) {
     chomp;
     $word = $_;
 
