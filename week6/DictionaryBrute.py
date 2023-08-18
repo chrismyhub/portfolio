dae6892b68a652393e48230999a3a329a663d5e1
chrismyhub <keyoch@gmail.com>
2023-08-18 04:51:52 +0000

    6.6 Step 1 Updated DictionaryBrute.py with "itertools.product" command and refactored repeating while loop code.

--- week6/DictionaryBrute.py
+++ week6/DictionaryBrute.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 #CREATED BY: Christopher Chong
 #DATE CREATED: 16AUG2023
-#DATE LAST MODIFIED: 17AUG2023
+#DATE LAST MODIFIED: 18AUG2023
 
 #Load Python's 'hashlib' module
 import hashlib
-#Load Python's 'random' module
-import random
 #Load 'datetime' data
 from datetime import datetime
+#Load 'intertools' data
+import itertools
 
 #Set variable startTime to time now 
 startTime = datetime.now()
@@ -19,98 +19,33 @@ data = "abcdefghijklmnopqrstuvwxyz"
 #Convert into array
 array = list(data)
 
-
+#
 #hidden password hash
-passwordHash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+passwordHash = "0ebdc3317b75839f643387d783535adc360ca01f33c75f7c1e7373adcd675c0b"
 #Passing through variable wordlistHash
 wordlistHash =""
-#Passing through variable k_num
-k_num=1
+k_num=0
 
 #While loop to iterate combination of characters/numbers
 while(wordlistHash != passwordHash):
 
-    #Length of word 1 character long
-    word = random.choices(array,k=k_num)
-    print(word)
-    word="".join(word)
+    #Add 1 to k_num variable each loop
+    k_num += 1   
+
+    #Start of "for" loop to begin finding combination/permutations of words.     
+    for group in itertools.product(array, repeat=k_num):
+        print(group)
+        word = group
+        word="".join(group)
+        print(word)
     
-    #hash the word
-    wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-    print(f"Trying password {word}:{wordlistHash}")
+        #hash the word
+        wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
+        print(f"Trying password {word}:{wordlistHash}")
 
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash):
-        print(f"Password has been cracked! It was {word}")
-        #Display the time taken to run the bruteforce
-        print(datetime.now() - startTime)
-        break
-        
-    #Length of word 2 character long
-    if wordlistHash != passwordHash:
-        word = random.choices(array,k=k_num + 1)
-    print(word)
-    word="".join(word)
-    #hash the word
-    wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-    print(f"Trying password {word}:{wordlistHash}")
-
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash):
-        print(f"Password has been cracked! It was {word}")
-        #Display the time taken to run the bruteforce
-        print(datetime.now() - startTime)
-        break
-        
-    #Length of word 3 character long
-    if wordlistHash != passwordHash:
-        word = random.choices(array,k=k_num + 2)
-    print(word)
-    word="".join(word)
-    
-    #hash the word
-    wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-    print(f"Trying password {word}:{wordlistHash}")
-
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash):
-        print(f"Password has been cracked! It was {word}")
-        #Display the time taken to run the bruteforce
-        print(datetime.now() - startTime)
-        break
-        
-    #Length of word 4 character long
-    if wordlistHash != passwordHash:
-        word = random.choices(array,k=k_num + 3)
-    print(word)
-    word="".join(word)
-    #hash the word
-    wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-    print(f"Trying password {word}:{wordlistHash}")
-
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash):
-        print(f"Password has been cracked! It was {word}")
-        #Display the time taken to run the bruteforce
-        print(datetime.now() - startTime)
-        break
-    
-    #Length of word 5 character long
-    if wordlistHash != passwordHash:
-        word = random.choices(array,k=k_num + 4)
-    print(word)
-    word="".join(word)
-    #hash the word
-    wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-    print(f"Trying password {word}:{wordlistHash}")
-
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash):
-        print(f"Password has been cracked! It was {word}")
-        #Display the time taken to run the bruteforce
-        print(datetime.now() - startTime)
-        break
-        
-        
-        
-       
+        #if the hash is the same as the correct password's hash then we have cracked the password!
+        if(wordlistHash == passwordHash):
+            print(f"Password has been cracked! It was {word}")
+            #Display the time taken to run the bruteforce
+            print(datetime.now() - startTime)
+            break
